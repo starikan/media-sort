2f7284b69d92aebc5d0f4e92e49db137b3a89395
sstarodubov <s.starodubov@litebox.ru>
2023-04-16 18:44:49 +0400

    camera sort

--- src/Helpers.ts
+++ src/Helpers.ts
@@ -3,27 +3,20 @@ import fs from 'fs';
 
 export const walkSync = (
   dir: string,
-  options: { ignoreFolders: string[]; extensions?: string[]; ignoreFiles: string[] } = {
-    ignoreFolders: [],
-    ignoreFiles: [],
-  },
+  options: { ignoreFolders?: string[]; extensions?: string[]; ignoreFiles?: string[] } = {},
 ): string[] => {
   const baseDir = path.basename(dir);
-
-  if (!fs.existsSync(dir) || options.ignoreFolders.includes(baseDir)) {
+  if (!fs.existsSync(dir) || (options?.ignoreFolders ?? []).includes(baseDir)) {
     return [];
   }
-
   if (!fs.statSync(dir).isDirectory()) {
     return [dir];
   }
-
   const dirs = fs
     .readdirSync(dir)
     .map((f) => walkSync(path.join(dir, f), options))
     .flat()
-    .filter((v) => !options.ignoreFiles.includes(v))
-    .filter((v) => (options.extensions ? options.extensions.includes(path.parse(v).ext) : true));
-
+    .filter((v) => !(options?.ignoreFiles ?? []).includes(v))
+    .filter((v) => (options?.extensions ?? []).includes(path.parse(v).ext.replace('.', '')));
   return dirs;
 };
---
+++ src/camerasSort.ts
@@ -0,0 +1,106 @@
+import fs from 'fs';
+import path from 'path';
+import dayjs from 'dayjs';
+import { walkSync } from './Helpers';
+
+export const getFilesFromRecorder = (dir: string = process.cwd()): string[] => {
+  const dirFull = path.resolve(dir);
+  const files = walkSync(dirFull, { extensions: ['mp4'] });
+  return files;
+};
+
+export const sortFilesFromRecorder = (dir: string = process.cwd()): void => {
+  getFilesFromRecorder(dir);
+};
+
+const parseDate = (dateString: string): Date => {
+  const year = parseInt(dateString.slice(0, 4), 10);
+  const month = parseInt(dateString.slice(4, 6), 10) - 1;
+  const day = parseInt(dateString.slice(6, 8), 10);
+  const hours = parseInt(dateString.slice(8, 10), 10);
+  const minutes = parseInt(dateString.slice(10, 12), 10);
+  const seconds = parseInt(dateString.slice(12, 14), 10);
+
+  const parsedDate = new Date(year, month, day, hours, minutes, seconds);
+
+  return parsedDate;
+};
+
+// Function to create a nested folder
+const createNestedFolder = (dirPath: string): void => {
+  const folders = dirPath.split(path.sep);
+  let currentPath = '';
+
+  folders.forEach((folder) => {
+    currentPath = path.join(currentPath, folder);
+    if (!fs.existsSync(currentPath)) {
+      fs.mkdirSync(currentPath);
+    }
+  });
+};
+
+const parseName = (
+  fileName: string,
+  exportFolder: string,
+): {
+  timestamp: Date;
+  timestampString: string;
+  camera: string;
+  source: string;
+  nameNew: string;
+  pathNew: string;
+  target: string;
+} => {
+  const fileNameClean = path.basename(fileName).replace(path.extname(fileName), '');
+  const fields = fileNameClean.split('_');
+  const timestamp = parseDate(fields[3]);
+  const timestampString = dayjs(timestamp).format('YYYY-MM-DD_HH-mm-ss');
+  const camera = fields[1];
+  const ext = path.extname(fileName);
+  const pathNew = path.join(
+    dayjs(timestamp).format('YYYY'),
+    dayjs(timestamp).format('YYYY-MM'),
+    dayjs(timestamp).format('YYYY-MM-DD'),
+  );
+  const nameNew = `${timestampString}[${camera}]${ext}`;
+  return {
+    timestamp,
+    timestampString,
+    camera,
+    source: fileName,
+    nameNew,
+    pathNew,
+    target: path.join(exportFolder, pathNew, nameNew),
+  };
+};
+
+const run = async (sourceFolder: string, exportFolder: string): Promise<void> => {
+  const files = getFilesFromRecorder(sourceFolder).map((v) => parseName(v, exportFolder));
+
+  if (!fs.existsSync(exportFolder)) {
+    fs.mkdirSync(exportFolder);
+  }
+
+  files
+    .map((v) => v.pathNew)
+    .filter((value, index, array) => array.indexOf(value) === index)
+    .map((v) => path.join(exportFolder, v))
+    .forEach((v) => createNestedFolder(v));
+
+  files.forEach((v, i) => {
+    console.log(i, v.source, v.target);
+    if (!fs.existsSync(v.target)) {
+      fs.copyFileSync(v.source, v.target);
+    } else {
+      if (!fs.existsSync(path.join(exportFolder, 'conflict'))) {
+        fs.mkdirSync(path.join(exportFolder, 'conflict'));
+      }
+      fs.copyFileSync(v.source, path.join(exportFolder, 'conflict', v.nameNew));
+    }
+  });
+
+  console.log('Done.');
+};
+
+run('F:\\', 'D:\\Фотки на синхронизацию\\Камеры\\EXPORT');
+// run('D:\\Фотки на синхронизацию\\Камеры\\НГ', 'D:\\Фотки на синхронизацию\\Камеры\\EXPORT_NG');
